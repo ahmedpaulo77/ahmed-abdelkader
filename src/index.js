e74282c30a419a812b75026ef8652c1b2f58bd93
ahmedpaulo77 <p638599@gmail.com>
2026-06-22 19:45:40 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// src/index.js
-import { LanguageProvider } from './contexts/LanguageContext';
-
-root.render(
-  <LanguageProvider>
-    <App />
-  </LanguageProvider>
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,7 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
